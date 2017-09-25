f8bb2f6f499d6d74e50c2a733d1d8247f38fa6ca
Dylan Taylor <i.djt93@gmail.com>
2017-09-25 14:18:44 -0400

    Add files via upload

---
+++ RunServer.bat
@@ -0,0 +1,9 @@
+@echo off
+color 20
+title Zoned Out RP Server
+echo Attempting to change directories
+cd /d D:\ZonedOutRP\cfx-server-data-master
+echo Success!
+echo Now executing Zoned Out RP Server
+echo Please wait a second...
+D:\ZonedOutRP\run.cmd +exec server.cfg
